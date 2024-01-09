d98435e98716fb440d18bf4601623748f928d2d1
nicosharpe <nicolesharpe11@gmail.com>
2024-01-09 16:00:02 +0000

    finalised script and added readme

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# random-password-generator
+# a-random-password-generator
 
-A Random Password Generator built using javascript.
+# A Random Password Generator built using javascript.
+
+I built this password generator to prompt the user, asking questions about desired password length and character types (uppercase, lowercase, numerical and symbols) and then based off the users preferences, the code then makes sure it includes at least one from each category and then fills in the rest at random from the requested categories until the character amount equals the amount the user requested.
+
+# Installation
+N/A
+
+# Usage
+To use this password generator, simply load up the webpage, click the 'Generate Password' button and add your preferences via the prompts. The numerical value for character length must be between 8 and 128 letters, there will be an error message if you go outside of this range and the prompts will begin again. To confirm wether or not you would like any of the other character types simply press "Ok" for yes or "cancel" for no. 
+
+Upon submitting your prompts a password will be returned which you can then copy and paste. 
+
+** Be sure to make note of and/or memorise your password **
+
+# Credits
+N/A
+
+# License
+Please refer to LICENSE in the reposition.
--- script.js
+++ script.js
@@ -89,20 +89,71 @@ var upperCasedCharacters = [
 ];
 
 // Function to prompt user for password options
-function getPasswordOptions() {
-alert("How long would you like your password to be? Must be at least 8 characters.")
 
+function getPasswordOptions() {
+  let passwordLength = parseInt(prompt("How many characters? Must be at least 8."));
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Sorry, must be a value between 8 and 128.")
+    return;
+  }
+
+  let characterTypeUpper = confirm("Would you like Uppercase characters?");
+  let characterTypeLower = confirm("Would you like Lower Case characters?");
+  let characterTypeNum = confirm("Would you like any numbers?");
+  let characterTypeSpecial = confirm("Would you like Special characters?");
+
+  return {
+    length: passwordLength,
+    upper: characterTypeUpper,
+    lower: characterTypeLower,
+    number: characterTypeNum,
+    special: characterTypeSpecial
+  }
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  const result = getPasswordOptions(); 
+  console.log("Result: ", result);
+  let requestedCharacters = [];
+
+
+  if (result.upper) {
+    requestedCharacters = requestedCharacters.concat(upperCasedCharacters);
+  }
+
+
+  if (result.lower) {
+    requestedCharacters = requestedCharacters.concat(lowerCasedCharacters);
+  }
+
+
+  if (result.number) {
+    requestedCharacters = requestedCharacters.concat(numericCharacters);
+  }
+
+  if (result.special) {
+    requestedCharacters = requestedCharacters.concat(specialCharacters);
+  }
+
+  let finalPassword = "";
+  for (let i = finalPassword.length; i < result.length; i++) { 
+    finalPassword += getRandom(requestedCharacters);
+  }
+
+  return finalPassword;
 
 }
+
+// based off users answers, combine whatever arrays they want 
+//and choose chosen arrays together and return length based off what they asked for
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -116,4 +167,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click', writePassword)
+
+
